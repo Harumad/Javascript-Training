959e0b4dd90fda04c01f94a4a0346e8f9fa721fd
harumad <rumadstar@gmail.com>
2025-09-16 21:39:56 +0000

    update git by starting js from scratch:veriable, data types and oprators

--- WEEK1/exercises/ex1.js
+++ WEEK1/exercises/ex1.js
@@ -1,8 +1,116 @@
-// Add two numbers
-function addNumbers(a, b) {
-  return a + b;
-}
 
-// Test the function
-let result = addNumbers(5, 7);
-console.log("The result is:", result);
+// variables
+let name ="Haruna";
+const country = 'Ghana';
+let age = 25;
+
+console.log(name);
+console.log(country);
+console.log(age);
+
+
+// data types
+let text ="I am learning Js in Codetrain"   //This is a string
+let num= 100                                // this is a number
+let isStudent =true;                        // This is bolean
+let emptyBox= null;                         //this is null
+let notSet ;                                // this is undefined
+
+
+console.log(text,num,isStudent,emptyBox,notSet);
+
+
+let a= 10;
+let b=3;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+// Exercise
+let myName = "Haruna";
+let myAge = 30;
+const myCountry = "Ghana"
+let student =true
+let hobby ="coding";
+let favFood = "jollof";
+let favColor = "blue";
+
+console.log(myName,myAge,myCountry, student);
+console.log(`my name is ${myName}, I am ${myAge} years old and I come from ${myCountry}.Am I a ${hobby} student of Codetrain? ${student}. My favourite food is ${favFood}.
+  My favorite color is ${favColor}`);
+
+
+// Arithmetic Operators
+let num1 =10;
+let num2 = 4;
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1/num2);
+console.log(num1*num2);
+console.log(num1%num2);
+
+
+let num3=15;
+let num4 = 7
+let sum =num3+num4
+console.log(`The sum of ${num3} and ${num4} is =${sum}`);
+
+let num5=20;
+let num6 = 9;
+let diff =num5-num6
+console.log(`The difference of ${num5} and ${num6} is =${diff}`);
+
+
+// Assingnment Operator
+let number1 =10;
+number1 +=5;
+console.log(number1);
+
+
+let number2 =10;
+number2 -=5;
+console.log(number2);
+
+let number3 =10;
+number3 *=5
+console.log(number3);
+
+let number4 =10;
+number4 /=5
+console.log(number4);
+
+let number5 =10;
+number5 %=5
+console.log(number5);
+
+// Assigment Exercise
+// You have ₵100 in your wallet. Do the following step by step using assignment operators (+=, -=, *=, /=,
+// Add ₵50 (maybe you found extra money).
+// Spend ₵30 on food.
+// Double the money you have left (side hustle).
+// Share the money equally among 4 friends.
+// Finally, check the remainder if you try to share the money among 7 friends.
+//  Print the result after each step.
+
+
+
+let wallet=100;
+wallet +=50;
+console.log(`out of the 100cedis in my wallet, i added 50cedis , making  me have a balance of${wallet} cedis`);
+
+
+// comparison operatros
+
+let tenants = 20;
+
+console.log(tenants === 20);
+console.log(tenants!==20);
+console.log(tenants !=20);
+console.log(tenants<=20);
+
+
